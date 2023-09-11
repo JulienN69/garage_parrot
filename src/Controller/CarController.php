947514285c23787eadb6c87fe179a53c6a533e6d
Julien Nesme <nesmejulien@yahoo.fr>
2023-09-11 22:36:26 +0200

    CRUD CarController

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Car;
 use App\Form\CarType;
 use App\Repository\CarRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,10 +22,22 @@ class CarController extends AbstractController
         ]);
     }
 
+// <----------------------- Start CRUD --------------------------->
+// <----------------------- CREATE --------------------------->
+
     #[Route('/car/create', name: 'car_create', methods:['GET', 'POST'])]
-    public function create(): Response
+    public function create(Request $request, CarRepository $carRepository, EntityManager $entitymanager): Response
     {
-        $form = $this->createForm(CarType::class);
+        $car = new Car();
+        $form = $this->createForm(CarType::class, $car);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $carRepository->save($car, true);
+            // $carRepository->persist($car);
+            // $carRepository->flush();
+            return $this->redirectToRoute('car');
+        }
 
         return $this->render('car/create.html.twig', [
             'formCar' => $form->createView()
@@ -31,26 +45,46 @@ class CarController extends AbstractController
 
     }
 
+    // <----------------------- READ --------------------------->
+
     #[Route('/car/{id}', name: 'car_display', methods:['GET'], requirements:['id'=>'\d+'])]
     public function display(CarRepository $carRepository,int $id): Response
     {
-         return $this->render('car/display.html.twig', [
-             'car' => ($carRepository->find($id))
-         ]);
+        return $this->render('car/display.html.twig', [
+            'car' => ($carRepository->find($id))
+        ]);
     }
 
-
+    // <----------------------- UPDATE --------------------------->
 
     #[Route('/car/{id}/update', name: 'car_update', methods:['GET', 'POST'], requirements:['id'=>'\d+'])]
-    public function update(): Response
-    {
-        dd(__METHOD__);
+public function update(int $id, Request $request, CarRepository $carRepository): Response
+{
+    $car = $carRepository->find($id);
+
+    $form = $this->createForm(CarType::class, $car);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $carRepository->save($car, true);
+
+        return $this->redirectToRoute('car');
     }
 
+    return $this->render('car/edit.html.twig', [
+        'formCar' => $form->createView()
+    ]);
+}
+
+    // <----------------------- DELETE --------------------------->
+
     #[Route('/car/{id}/delete', name: 'car_delete', methods:['GET'], requirements:['id'=>'\d+'])]
-    public function delete(): Response
-    {
-        dd(__METHOD__);
+    public function delete(int $id, CarRepository $carRepository) : Response {
+
+        $car = $carRepository->find($id);
+
+        $carRepository->remove($car, true);
+        return $this->redirectToRoute('car');
     }
     
 }
--- src/Form/CarType.php
+++ src/Form/CarType.php
@@ -4,21 +4,47 @@ namespace App\Form;
 
 use App\Entity\Car;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 
 class CarType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('mileage')
-            ->add('dateCreation')
-            ->add('price')
-            ->add('modele')
+            ->add('mileage', IntegerType::class, [
+                'label' => 'kilométrage'
+            ], [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir une marque',
+                    ]),
+                ],
+                'required' => false,
+            ])
+            ->add('dateCreation', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de mise en service'
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'prix'
+            ])
+            ->add('modele', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir une marque',
+                    ]),
+                ],
+                'required' => false,
+            ])
             // ->add('is_equipped')
-            ->add('submit', SubmitType::class)
+            ->add('valider', SubmitType::class)
         ;
     }
 
